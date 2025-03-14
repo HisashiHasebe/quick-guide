fb69d3f971fe314675751f0540c8024ece4de015
HisashiHasebe <hhasebe@diverta.co.jp>
2025-03-14 13:43:55 +0800

    fix:preview

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,67 +1,29 @@
-'use client';
+'use client'
 
-import { Box, Stack, Typography } from '@mui/material';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation'
 
-const PreviewPage = () => {
-  const searchParams = useSearchParams();
-  
-  type Topic = {
-    subject: string;
-    contents: string;
-  };
-  
-  const [topic, setTopic] = useState<Topic | null>(null);
+export default function Page() {
+  const searchParams = useSearchParams()
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const token = searchParams.get('preview_token');
-    if (!token) return;
-
-    fetch(
-      process.env.NEXT_PUBLIC_BASE_URL +
-        '/rcms-api/1/content/preview?preview_token=' +
-        token,
-      {
-        method: 'GET',
-        credentials: 'include',
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.errors && data.errors.length) {
-          return;
-        }
-        setTopic(data.details);
-      });
-  }, [searchParams]);
-
-  if (!topic) {
-    return null;
+    const token = searchParams.get('preview_token')
+    const fetchData = async () => {
+        const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL + '/rcms-api/1/content/preview?preview_token=' + token).then((res) => res.json());
+        setData(res);
+    };
+    fetchData();
+  }, [searchParams]); 
+  
+  if (data === null) {
+    return <div>Loading...</div>;
   }
 
   return (
-    <Stack spacing={2}>
-      <Box
-        sx={{
-          background: '#333',
-          color: 'white',
-          p: 2,
-          borderRadius: '8px',
-        }}
-      >
-        Preview
-      </Box>
-      <Typography variant="h5" component="h1">
-        {topic.subject}
-      </Typography>
-      <Typography
-        dangerouslySetInnerHTML={{
-          __html: topic.contents,
-        }}
-      />
-    </Stack>
+    <div>
+      <h1>{data.details.subject}</h1>
+      <div dangerouslySetInnerHTML={{ __html: data.details.contents }} />
+    </div>
   );
-};
-
-export default PreviewPage;
+}
